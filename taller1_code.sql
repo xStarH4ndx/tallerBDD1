1d00a1e4c3e14bcb6472ea18e05a850fba4a6601
broken_Arm <brunotoro1799@gmail.com>
2024-04-24 23:00:24 -0400

    consulta 1

--- taller1_code.sql
+++ taller1_code.sql
@@ -10,6 +10,7 @@ DROP TABLE IF EXISTS espacio;
 DROP TABLE IF EXISTS cliente;
 */
 
+/*---------POBLAR BASE DE DATOS--------*/
 create table cliente(
 	rut_cliente text primary key,
 	nombre_cliente text not null,
@@ -44,7 +45,7 @@ create table contrato(
 
 
 -- Insertar datos en la tabla cliente
-INSERT INTO cliente (rut_cliente, nombre_cliente, direccion, correo, telefono)VALUES
+insert into cliente (rut_cliente, nombre_cliente, direccion, correo, telefono)VALUES
 ('12345678-9', 'Juan Pérez', 'Calle 123', 'juan@ucn.com', '+56912345678'),
 ('98765432-1', 'María Gómez', 'Avenida Principal', 'maria@ucn.com', '+56987654321'),
 ('55555555-5', 'Pedro Rodríguez', 'Avenida Central', 'pedro@ucn.com', '+56955555555'),
@@ -52,7 +53,7 @@ INSERT INTO cliente (rut_cliente, nombre_cliente, direccion, correo, telefono)VA
 ('99999999-9', 'Laura López', 'Calle Principal', 'laura@ucn.com', '+56999999999');
 
 -- Insertar datos en la tabla espacio
-INSERT INTO espacio (tipo_espacio, capacidad_max, ubicacion, servicios_disponibles, tarifas)VALUES
+insert into espacio (tipo_espacio, capacidad_max, ubicacion, servicios_disponibles, tarifas)VALUES
 ('Salón de conferencias', 100, 'Piso 1', 'Proyector, equipo de sonido, catering', '1000'),
 ('Área al aire libre', 200, 'Jardín', 'Catering, mobiliario', '1500'),
 ('Sala de reuniones', 20, 'Piso 2', 'Proyector, pizarra, coffee break', '500'),
@@ -60,7 +61,7 @@ INSERT INTO espacio (tipo_espacio, capacidad_max, ubicacion, servicios_disponibl
 ('Sala de exposiciones', 50, 'Piso 4', 'Mobiliario, iluminación', '800');
 
 -- Insertar datos en la tabla reserva
-INSERT INTO reserva (id_reserva, tipo_espacio, servicios_adicionales, cantidad_asistentes, hora, fecha_reserva)VALUES
+insert into reserva (id_reserva, tipo_espacio, servicios_adicionales, cantidad_asistentes, hora, fecha_reserva)VALUES
 (1, 'Salón de conferencias', 'Proyector, equipo de sonido', 80, '09:00', '2024-04-20'),
 (2, 'Área al aire libre', 'Catering', 150, '14:00', '2024-05-15'),
 (3, 'Sala de reuniones', 'Proyector, coffee break', 15, '11:00', '2024-06-10'),
@@ -68,7 +69,7 @@ INSERT INTO reserva (id_reserva, tipo_espacio, servicios_adicionales, cantidad_a
 (5, 'Sala de exposiciones', 'Mobiliario', 30, '15:30', '2024-08-20');
 
 -- Insertar datos en la tabla contrato
-INSERT INTO contrato (id_reserva, rut_cliente, fecha_contrato, precio)VALUES
+insert into contrato (id_reserva, rut_cliente, fecha_contrato, precio)VALUES
 (1, '12345678-9', '2024-04-19', 1200),
 (2, '98765432-1', '2024-04-20', 1800),
 (3, '55555555-5', '2024-04-21', 700),
@@ -76,4 +77,11 @@ INSERT INTO contrato (id_reserva, rut_cliente, fecha_contrato, precio)VALUES
 (5, '99999999-9', '2024-04-23', 1000);
 
 
+/*---------------CONSULTAS---------------*/
+--1
+select e.tipo_espacio, e.capacidad_max, e.ubicacion, e.servicios_disponibles, e.tarifas
+from espacio as e
+inner join reserva as r on e.tipo_espacio = r.tipo_espacio
+where r.fecha_reserva != '2024-06-10'
+order by e.capacidad_max asc
 
